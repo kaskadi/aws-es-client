ff1604454a86db10f96b69f7e34b000d4d037cf4
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-19 12:48:25 +0100

    Imported code from implementation with new ElasticSearch client

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+const { createAWSConnection, awsCredsifyAll } = require('@acuris/aws-es-connection')
+const { Client } = require('@elastic/elasticsearch')
+const AWS = require('aws-sdk')
+
 module.exports = function createClient (opt) {
   var options = {...{region: 'eu-central-1'}, ...opt}
-  var elastic = require('elasticsearch')
-  const AWS = require('aws-sdk')
   AWS.config.update({
     credentials: new AWS.Credentials(options.id, options.token),
     region: options.region
   })
-  var client = new elastic.Client({
-    hosts: [options.url],
-    connectionClass: require('http-aws-es')
-  })
-  return client
+  return awsCredsifyAll(
+    new Client({
+      node: options.url,
+      Connection: createAWSConnection(AWS.config.credentials)
+    })
+  )
 }
-
-// force
