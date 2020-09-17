7a106879de134364b6add1f85f74797d7b7f20fb
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-17 13:00:41 +0200

    Added comments for automatic documentation

--- index.js
+++ index.js
@@ -2,8 +2,39 @@ const { createAWSConnection, awsCredsifyAll } = require('@acuris/aws-es-connecti
 const { Client } = require('@elastic/elasticsearch')
 const AWS = require('aws-sdk')
 
-module.exports = function createClient (opt) {
-  var options = {...{region: 'eu-central-1'}, ...opt}
+/**
+ * @module aws-es-client
+ * @typicalname ES
+ */
+
+/**
+ * Options for the new ElasticSearch client instanciation
+ * @typedef {Object} Options
+ * @property {string} [region='eu-central-1'] - The region in which your ElasticSearch cluster lives
+ * @property {string} url - The URL of your ElasticSearch cluster on AWS
+ * @property {string} id - The ID of a programmatic user having access to ElasticSearch service on AWS
+ * @property {string} token - The token of a programmatic user having access to ElasticSearch service on AWS
+ */
+
+/**
+ * Creates a new client connected to your AWS ElasticSearch cluster. 
+ * 
+ * Once the client has been instanciated, you can use all the methods available in the regular ElasticSearch Node client (see [here](https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html) for details)
+ * @function
+ * @param {Options} opts - {@link Options} to be passed to instanciate a new client
+ * @return {ES} New ElasticSearch client
+ *
+ * @example
+ *
+ * const ES = require('aws-es-client')({
+ *  url: process.env.ES_CLUSTER_URL,
+ *  id: process.env.ES_ID,
+ *  token: process.env.ES_SECRET
+ * })
+ */
+
+module.exports = function createClient (opts) {
+  const options = {...{region: 'eu-central-1'}, ...opts}
   AWS.config.update({
     credentials: new AWS.Credentials(options.id, options.token),
     region: options.region
