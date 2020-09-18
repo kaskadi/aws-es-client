997ed8ceca52208f59680ae6e45c0c4af4fcd1cd
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-18 14:46:01 +0200

    Fixed links

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const AWS = require('aws-sdk')
 /**
  * Creates a new client connected to your AWS ElasticSearch cluster. 
  * 
- * Once the client has been instanciated, you can use all the methods available in the regular ElasticSearch Node client (see [here]{@link https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html} for details)
+ * Once the client has been instanciated, you can use all the methods available in the regular ElasticSearch Node client (see {@link https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html|here} for details)
  * 
  * @module aws-es-client
  * 
- * @param {Options} opts - [Options]{@link Options} to be passed to instanciate a new client
+ * @param {Options} opts - {@link Options} to be passed to instanciate a new client
  * @return {ES} New ElasticSearch client
  *
  * @example
