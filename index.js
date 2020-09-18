55158ba7c7a679400d32065f5348be7d1ca9d8f3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-18 13:44:33 +0200

    Fixed/cleaned up docs

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const { Client } = require('@elastic/elasticsearch')
 const AWS = require('aws-sdk')
 
 /**
- * A module to generate a new client for your AWS ElasticSearch cluster.
- * 
- * @module aws-es-client
- * @typicalname ES
- */
-
-/**
  * ElasticSearch client options
  * @typedef {Object} Options
  * @property {string} [region=eu-central-1] - ElasticSearch cluster region
@@ -21,9 +14,11 @@ const AWS = require('aws-sdk')
 /**
  * Creates a new client connected to your AWS ElasticSearch cluster. 
  * 
- * Once the client has been instanciated, you can use all the methods available in the regular ElasticSearch Node client (see [here](https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html) for details)
+ * Once the client has been instanciated, you can use all the methods available in the regular ElasticSearch Node client (see [here]{@link https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html} for details)
  * 
- * @param {Options} opts - [Options]{@link module:aws-es-client~Options} to be passed to instanciate a new client
+ * @module aws-es-client
+ * 
+ * @param {Options} opts - [Options]{@link Options} to be passed to instanciate a new client
  * @return {ES} New ElasticSearch client
  *
  * @example
